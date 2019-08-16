504558865efb7df0f833df15114428b8361a86cc
SathishSaminathan <Sathishcse1975@gmail.com>
2019-08-16 18:34:43 +0530

    commit for fixes

--- src/components/PaginationComponent/index.js
+++ src/components/PaginationComponent/index.js
@@ -3,16 +3,55 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 
 class PaginationComponent extends Component {
+  state = {
+    activeItem: 5,
+    prevIndex: null,
+    total: 20
+  };
+  componentDidMount() {
+    this.slider.slickGoTo(this.state.activeItem - 1);
+  }
   renderItems = () => {
+    const { activeItem, total } = this.state;
     let template = [];
-    for (let i = 1; i <= 20; i++) {
+    for (let i = 1; i <= total; i++) {
       template.push(
-        <div className="sample" onClick={() => console.log(i)}>
-          <h3>{i}</h3>
+        <div
+          className="sample"
+          onClick={() =>
+            this.setState({
+              activeItem: i
+            })
+          }
+          key={i}
+        >
+          <div className={`textArea ${activeItem === i ? "active" : ""}`}>
+            <span>{i}</span>
+          </div>
         </div>
       );
     }
     return template;
+  };
+  afterChange = currentIndex => {
+    console.log("currentIndex", currentIndex);
+    const { prevIndex } = this.state;
+    if (currentIndex > prevIndex) {
+      this.setState({
+        activeItem: this.state.activeItem + 1
+      });
+    } else {
+      this.setState({
+        activeItem: this.state.activeItem - 1
+      });
+    }
+    console.log("activeItem", this.state.activeItem);
+  };
+  beforeChange = prevIndex => {
+    console.log("prevIndex", prevIndex);
+    this.setState({
+      prevIndex
+    });
   };
   render() {
     const settings = {
@@ -20,7 +59,11 @@ class PaginationComponent extends Component {
       infinite: true,
       speed: 500,
       slidesToShow: 10,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      afterChange: this.afterChange,
+      beforeChange: this.beforeChange,
+      swipeToSlide: true
+      // focusOnSelect: true,
     };
     return (
       <div
@@ -29,11 +72,12 @@ class PaginationComponent extends Component {
           position: "absolute",
           top: "50%",
           left: "50%",
-          transform: "translate(-50%, -50%)",
-          backgroundColor: "red"
+          transform: "translate(-50%, -50%)"
         }}
       >
-        <Slider {...settings}>{this.renderItems()}</Slider>
+        <Slider ref={c => (this.slider = c)} {...settings}>
+          {this.renderItems()}
+        </Slider>
       </div>
     );
   }
